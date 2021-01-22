293f97f0ff7b462659fbcee9f5baf6eb7df80fe6
ytyaru <ytyaru@outlook.jp>
2021-01-22 14:18:10 +0900

    remove file

--- docs/5/ThreeButtonComponent.js
+++
@@ -1,9 +0,0 @@
-// Vue.createApps().components
-var ThreeButtonComponent = {
-    components: {
-        'button-counter-1': ButtonComponent,
-        'button-counter-2': ButtonComponent,
-        'button-counter-3': ButtonComponent,
-    }
-};
-
